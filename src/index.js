74a49c0642275189c73bfd2781366d29ce7dc2ab
Gautam Mehar <gautammehar792@gmail.com>
2025-09-12 17:57:39 +0530

    connect to db

---
+++ src/index.js
@@ -0,0 +1,62 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async ()=>{
+    try{
+
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    }
+    catch(eror)
+    {
+        console.error("Error", error);
+        throw err
+        
+
+    }
+    
+
+    
+})()
+    */
